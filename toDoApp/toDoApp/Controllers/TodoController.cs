a0b05883258ccb6fa7186f9d4a623406454215f3
bryanbalderas <bryanpbalderas@gmail.com>
2021-12-15 18:50:38 -0600

    primerosArchivos

---
+++ toDoApp/toDoApp/Controllers/TodoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace toDoApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        public String Index()
+        {
+            return "test";
+        }
+    }
+}
